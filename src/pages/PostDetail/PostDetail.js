cd53ac2f91c6ab3fe6402d5af5d5b0deb9d8df45
Sterba Patrik <sterba@cngroup.dk>
2020-03-26 16:58:55 +0100

    useEffect added

--- src/pages/PostDetail/PostDetail.js
+++ src/pages/PostDetail/PostDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../../components/Layout/Layout';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
@@ -6,39 +6,52 @@ import {
   faRetweet,
   faExternalLinkAlt,
 } from '@fortawesome/free-solid-svg-icons';
+import { useParams } from 'react-router-dom';
+
+import tweets from '../../mocks/tweets.json';
 
 import './PostDetail.css';
 
 const PostDetail = () => {
+  const { id } = useParams();
+  const [specificTweet, setSpecificTweet] = useState({});
+
+  useEffect(() => {
+    if (id) {
+      const tweet = tweets.find(tweet => tweet.id === parseInt(id));
+
+      setSpecificTweet(tweet);
+    }
+  }, [id]);
+
   return (
-    <Layout>
-      <div className='post-detail'>
-        <div className='post-detail__header'>
-          <img
-            src='https://randomuser.me/api/portraits/women/11.jpg'
-            alt='Random user'
-          />
-          <div className='post-detail__header-name'>
-            <strong>Some Interesting Guy</strong>
-            <span>@someInterestingGuy</span>
+    <>
+      {specificTweet && (
+        <Layout>
+          <div className='post-detail'>
+            <div className='post-detail__header'>
+              <img
+                src='https://randomuser.me/api/portraits/women/11.jpg'
+                alt='Random user'
+              />
+              <div className='post-detail__header-name'>
+                <strong>{specificTweet.user}</strong>
+                <span>@{specificTweet.user}</span>
+              </div>
+            </div>
+            <div className='post-detail__content'>
+              <div className='post__message'>{specificTweet.body}</div>
+              <div className='post-detail__action-buttons'>
+                <FontAwesomeIcon icon={faComment} />
+                <FontAwesomeIcon icon={faRetweet} />
+                <FontAwesomeIcon icon={faHeart} />
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className='post-detail__content'>
-          <div className='post__message'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
-            exercitationem vitae qui maxime facere vero animi dicta? Dolorem
-            alias repellendus impedit et eligendi nulla repudiandae delectus
-            explicabo? Blanditiis, asperiores ex.
-          </div>
-          <div className='post-detail__action-buttons'>
-            <FontAwesomeIcon icon={faComment} />
-            <FontAwesomeIcon icon={faRetweet} />
-            <FontAwesomeIcon icon={faHeart} />
-            <FontAwesomeIcon icon={faExternalLinkAlt} />
-          </div>
-        </div>
-      </div>
-    </Layout>
+        </Layout>
+      )}
+    </>
   );
 };
 
