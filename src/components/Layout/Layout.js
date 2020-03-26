c54cc1e6c7fc3a2e2ada3fd19451bb9002393e32
Sterba Patrik <sterba@cngroup.dk>
2020-03-26 17:07:41 +0100

    useLocation added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import Logo from '../Logo/Logo';
 
 const Layout = ({ children }) => {
+  const { pathname } = useLocation();
+
   return (
     <div className='App'>
       <Logo />
       <div className='container'>
-        <h1>Main page</h1>
+        {pathname === '/' ? <h1>Main page</h1> : <h1>Post</h1>}
         {children}
       </div>
     </div>
