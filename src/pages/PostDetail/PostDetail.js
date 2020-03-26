279413f6b1de9f3dd702786876c914eb9b368f67
Sterba Patrik <sterba@cngroup.dk>
2020-03-26 15:47:13 +0100

    Post detail added

--- src/pages/PostDetail/PostDetail.js
+++ src/pages/PostDetail/PostDetail.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import Layout from '../../components/Layout/Layout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
+import {
+  faRetweet,
+  faExternalLinkAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 const PostDetail = () => {
-  const { goBack } = useHistory();
-
   return (
-    <div>
-      <button onClick={() => goBack()}>Go back</button> Post detail...
-    </div>
+    <Layout>
+      <div className='post-detail'>
+        <img
+          src='https://randomuser.me/api/portraits/women/11.jpg'
+          alt='Random user'
+        />
+        <div className='post-detail__header-name'>
+          <strong>Some guy</strong>
+          <span>@someGuy</span>
+        </div>
+      </div>
+
+      <div className='post-detail__content'>
+        <div className='post__message'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+          adipisci pariatur cumque ipsum fugiat repellendus, odit ut cupiditate
+          sunt sequi asperiores earum magni eligendi sapiente ullam illo
+          reiciendis! Corrupti, ullam.
+        </div>
+        <div className='post-detail__action-buttons'>
+          <FontAwesomeIcon icon={faComment} />
+          <FontAwesomeIcon icon={faRetweet} />
+          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon icon={faExternalLinkAlt} />
+        </div>
+      </div>
+    </Layout>
   );
 };
 
