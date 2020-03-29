ef0f687b85f70ccc1ca52cbae4de74946e82331c
Matus Giertl <matus1giertl@gmail.com>
2020-03-29 17:04:42 +0200

    reformatted

--- src/pages/PostDetail/PostDetail.js
+++ src/pages/PostDetail/PostDetail.js
@@ -1,57 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import Layout from '../../components/Layout/Layout';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
 import {
   faRetweet,
-  faExternalLinkAlt,
-} from '@fortawesome/free-solid-svg-icons';
-import { useParams } from 'react-router-dom';
+  faExternalLinkAlt
+} from "@fortawesome/free-solid-svg-icons";
+import { useParams } from "react-router-dom";
 
-import tweets from '../../mocks/tweets.json';
+import tweets from "../../mocks/tweets.json";
 
-import './PostDetail.css';
+import "./PostDetail.css";
 
 const PostDetail = () => {
   const { id } = useParams();
-  const [specificTweet, setSpecificTweet] = useState({});
 
-  useEffect(() => {
-    if (id) {
-      const tweet = tweets.find(tweet => tweet.id === parseInt(id));
-
-      setSpecificTweet(tweet);
-    }
-  }, [id]);
+  const tweet = tweets.find(tweet => tweet.id === parseInt(id));
+  const { user, body } = tweet;
 
   return (
-    <>
-      {specificTweet && (
-        <Layout>
-          <div className='post-detail'>
-            <div className='post-detail__header'>
-              <img
-                src='https://randomuser.me/api/portraits/women/11.jpg'
-                alt='Random user'
-              />
-              <div className='post-detail__header-name'>
-                <strong>{specificTweet.user}</strong>
-                <span>@{specificTweet.user}</span>
-              </div>
-            </div>
-            <div className='post-detail__content'>
-              <div className='post__message'>{specificTweet.body}</div>
-              <div className='post-detail__action-buttons'>
-                <FontAwesomeIcon icon={faComment} />
-                <FontAwesomeIcon icon={faRetweet} />
-                <FontAwesomeIcon icon={faHeart} />
-                <FontAwesomeIcon icon={faExternalLinkAlt} />
-              </div>
-            </div>
+    <Layout>
+      <div className="post-detail">
+        <div className="post-detail__header">
+          <img
+            src="https://randomuser.me/api/portraits/women/11.jpg"
+            alt="Random user"
+          />
+          <div className="post-detail__header-name">
+            <strong>{user}</strong>
+            <span>@{user}</span>
           </div>
-        </Layout>
-      )}
-    </>
+        </div>
+        <div className="post-detail__content">
+          <div className="post__message">{body}</div>
+          <div className="post-detail__action-buttons">
+            <FontAwesomeIcon icon={faComment} />
+            <FontAwesomeIcon icon={faRetweet} />
+            <FontAwesomeIcon icon={faHeart} />
+            <FontAwesomeIcon icon={faExternalLinkAlt} />
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 };
 
